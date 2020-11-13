27b469f676d381959affab9fd48d1623d16b07b6
Caleb Smith <smithhouse@Calebs-MacBook-Pro.local>
2020-11-13 11:23:47 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
+class App extends Component {
+
+state = {
+  loading: true,
+  person: null
+}
+
+async componentDidMount (){
+  const url = "https://api.randomuser.me/";
+  const request = await fetch(url);
+  const data = await request.json();
+  this.setState({
+    loading: false,
+    person: data.results[0]
+  });
+}
+
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Random Users</h1>
+    {this.state.loading || !this.state.person ?
+    (<div>loading...</div>) :
+    (<div>
+  <div>{this.state.person.name.first}</div>
+  <div>{this.state.person.name.last}</div>
+  <img src={this.state.person.picture.large}/>
+  <div>{this.state.person.location.state}, {this.state.person.location.country}</div>
+  </div>)
+}
+</div>
+
   );
+}
 }
 
 export default App;
